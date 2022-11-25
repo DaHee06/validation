d2d9e0ec28a1164ffc1d9e0452d46b01d329e151
DaHee06 <todayda1006@gmail.com>
2022-11-25 21:06:00 +0900

    section4- 검증 마무리, @validated 적용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,12 +2,19 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ItemServiceApplication {
+public class ItemServiceApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	//글로벌 설정
+//	@Override
+//	public Validator getValidator() {
+//		return WebMvcConfigurer.super.getValidator();
+//	}
 }
